6c3fc5292699d3d8f166b22d32539aa2e05a04dd
Pira <sritharan.pirathaab@gmail.com>
2018-07-31 21:36:48 +0200

    docker-mysql-node project

---
+++ mysql-node/src/config.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import convict from 'convict';
+import * as fs from 'fs';
+
+// Define a schema
+const config = convict({
+    env: {
+        doc: "The application environment.",
+        format: ["production", "development", "test"],
+        default: "development",
+        env: "NODE_ENV"
+    },
+    port: {
+        doc: "The port to bind.",
+        format: "port",
+        default: 4000,
+        env: "PORT",
+        arg: "port"
+    },
+    db: {
+        host: {
+            doc: "Database host name/IP",
+            format: '*',
+            default: 'db',
+            env: 'MYSQL_HOST'
+        },
+        name: {
+            doc: "Database name",
+            format: String,
+            default: 'database',
+            env: 'MYSQL_NAME'
+        },
+        user: {
+            doc: "Database username",
+            format: String,
+            default: 'root',
+            env: 'MYSQL_USER'
+        },
+        password: {
+            doc: "Database password",
+            format: String,
+            default: 'password',
+            env: 'MYSQL_PASSWORD'
+        }
+    },
+    redis: {
+        host: {
+            doc: "Redis host",
+            format: String,
+            default: 'redis',
+            env: 'REDIS_HOST'
+        },
+    }
+});
+
+// Load environment dependent configuration
+const env = config.get('env');
+const envFile = './config/' + env + '.json'
+if (fs.existsSync(envFile)) {
+    config.loadFile(envFile);
+}
+
+// Perform validation
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
---
+++ mysql-node/src/server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import express from "express";
+import * as config from "./config";
+
+// Init express
+const app = express();
+
+// Init MySQL Connection
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: config.get('db.host'),
+  user: config.get('db.user'),
+  password: config.get('db.password'),
+  database: config.get('db.name')
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('error during db conneciton', err);
+    } else {
+        console.log("You're connected to your db !");
+    }
+});
+
+// Init redis connection
+const redis = require("redis"),
+      client = redis.createClient({ host: config.get('redis.host') });
+
+client.on("error", (err) => {
+  console.error("Error " + err);
+});
+
+client.set("foo", "bar", redis.print);
+
+// Init test routes
+
+app.get("/", (req, res) => {
+  res.send(`Hello World from : ${req.connection.localAddress}`);
+});
+
+app.get("/db", (req, res) => {
+  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) {
+      console.error('error in /db', error);
+    }
+    return res.json(results);
+  });  
+})
+
+app.get("/redis", (req, res) => {
+  client.get('foo', (err, reply) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json({
+      key: 'foo',
+      value: reply
+    });
+  })
+});
+
+// Init API listening
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
