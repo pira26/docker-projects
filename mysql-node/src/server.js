0a962dc45ded1671dd412df5ef2a8f43d1d9e355
Pira <sritharan.pirathaab@gmail.com>
2018-08-25 18:00:51 +0200

    docker-mysql-node project

--- mysql-node/src/server.js
+++ mysql-node/src/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require("express");
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const config = require("./config");
 
 // Init express
@@ -12,42 +14,74 @@ const client = redis.createClient({ host: config.redisHost });
 
 // Init MySQL Connection
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: config.mysqlHost,
   user: config.user,
   password: config.password,
   database: config.db,
-  port: config.portDb
+  port: config.portDb,
+  connectionLimit: 10
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('error during db connection', err);
-    } else {
-        console.log("You're connected to your db !");
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
     }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
+    }
+  }
+
+  if (connection) {
+    connection.release();
+  }
+
+  return;
 });
 
-// client.on("error", (err) => {
-//   console.error("Error " + err);
-// });
+client.on("error", (err) => {
+  console.error("Error " + err);
+});
 
 client.set("foo", "bar", redis.print);
 
-// Init test routes
+app.use(logger('dev'));
 
+// Set header
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.disable('x-powered-by');
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, PATCH, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+
+});
+
+// Init test routes
 app.get("/", (req, res) => {
-  console/log("url route '/'");
+  console.log("url route '/'");
   res.send(`Hello World from : ${req.connection.localAddress}`);
 });
 
 app.get("/db", (req, res) => {
-  connection.query(`SELECT * FROM Test`, (error, results, fields) => {
+  pool.query(`SELECT * FROM Test`, (error, results, fields) => {
     if (error) {
       console.error('error in /db', error);
     } else {
-      console.log('results -> ', results);
-      console.log('fieds ==> ', fields);
       return res.json(results);
     }
   });  
